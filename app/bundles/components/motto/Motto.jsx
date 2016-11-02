fafcdb527d637ab8d864300be84b69bab7bda5e2
lian <liushuanglian@cardvalue.cn>
2016-11-02 15:59:44 +0800

    merge

--- app/bundles/components/motto/Motto.jsx
+++ app/bundles/components/motto/Motto.jsx
@@ -4,10 +4,6 @@ import MottoBox from './MottoBox';
 
 class Motto extends React.Component{
   constructor(){
-    // 构造函数里面做的任务就是默认的state以及一些函数方法的绑定
-    // 如果再render里面绑定方法，是不是要考虑一下每次render都会进行绑定一次，
-    // 没有看过源码，按照react的算法，应该不可能，可能是多虑了，不过在constructor里面
-    // 绑定this，是官方推荐的做法
     super();
     this.state = {
       liked: false,
@@ -15,7 +11,6 @@ class Motto extends React.Component{
       mottoJap: '桜があんなに洁く散るのは来年も咲くのわかってるからだよ',
       mottoChina: '樱花这么毅然飘散是因为她知道明年还会再开'
     };
-    this.handleExpChange = this.handleExpChange.bind(this)
     this.toggleChange = this.toggleChange.bind(this)
   }
 
@@ -23,23 +18,12 @@ class Motto extends React.Component{
     this.setState({liked: !this.state.liked})
   }
 
-  handleExpChange(e){
-    this.setState({expression: e.target.value})
-  }
-  // 这边可以使用一个通用的handleChange方法，目前的方法，可以用到一些有value值得情况下，
-  // 碰到一些时间空间或者其他选择控件或许会出问题
-  // 并且如果一个组建里面有很多的input那按照这个就会写好多相同的方法了。
-  // 优化一下如下
   handleChange(files, e) {
     this.setState({ [files]: e && e.target ? e.target.value : e })
   }
 
   render() {
-    // 使用es6解构，代码更加优雅，便于维护
-    const { mottoJap, mottoChina, expression, liked } = this.state
-    // const mottoJap = this.state.mottoJap;
-    // const mottoChina = this.state.mottoChina;
-
+    const { mottoJap, mottoChina, expression, liked } = this.state;
     return (
         <section>
           <Grid>
@@ -50,7 +34,6 @@ class Motto extends React.Component{
                     myExp={expression}
                     myMotto={liked && mottoChina || mottoJap}
                     updateExpression={this.handleChange.bind(this, 'expression')}
-                    // updateExpression={this.handleExpChange}
                     switchMotto={this.toggleChange}
                   />
                 </div>
--- app/bundles/components/motto/MottoBox.jsx
+++ app/bundles/components/motto/MottoBox.jsx
@@ -3,8 +3,7 @@ import { Grid,Row,Col, FormControl } from 'react-bootstrap';
 
 class MottoBox extends React.Component {
   render() {
-    // 同样适用解构的方法，不然维护要到render里面去找，文件变大会变得难以维护
-    const { switchMotto, myMotto, myExp, updateExpression } = this.props
+    const { switchMotto, myMotto, myExp, updateExpression } = this.props;
     return (
       <Grid>
         <Row>
